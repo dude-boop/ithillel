c1e4a9e01a46d6c13271012d52d3ab3a1d873285
dude-boop <psihnofrost@gmail.com>
2024-10-15 21:29:22 +0300

    Merge pull request #4 from dude-boop/detached3
    simple_calculator.py the first part of third home work

---
+++ simple_calculator.py
@@ -0,0 +1,24 @@
+num_1 = int(input("Enter your first number: "))
+num_2 = int(input("Enter your second number: "))
+answer = int(input("""
+    Enter your type of operation: 
+    1 +
+    2 -
+    3 /
+    4 *
+    """))
+
+
+if answer == 1:
+    print(num_1 + num_2)
+elif answer == 2:
+    print(num_1 - num_2)
+elif answer == 3:
+    if num_2 != 0:
+        print(num_1 / num_2)
+    else:
+        print("division by zero, try another numbers")
+elif answer == 4:
+    print(num_1 * num_2)
+else:
+    print("Invalid input")
