d9150b0d028cb4c32f078e125e2ea17e7590911a
dude-boop <psihnofrost@gmail.com>
2024-10-15 21:39:32 +0300

    Merge pull request #4 from dude-boop/detached3
    last_and_first_symbol_of_list patched

--- last_and_first_symbol_of_list.py
+++ last_and_first_symbol_of_list.py
@@ -1,8 +1,5 @@
-from random import randint
-some_list = [randint(0, 100) for i in range(15)]
-print(some_list)
+some_list = [3,18,44,89,300]
 some_list_first_symbol = some_list[0]
 some_list_last_symbol = some_list[-1]
 some_list[0] = some_list_last_symbol
 some_list[-1] = some_list_first_symbol
-print(some_list)
