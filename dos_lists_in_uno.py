2251da4e26f78861a6adcc884eb10c026b25d143
dude-boop <psihnofrost@gmail.com>
2024-10-18 20:28:06 +0300

    hw3.3

---
+++ dos_lists_in_uno.py
@@ -0,0 +1,4 @@
+big_list = ["uno", "dos", "tres", "quatro", "cinco", "seis", "sete"]
+first_part_of_big_list = big_list[:len(big_list)//2]
+second_part_of_big_list = big_list[len(big_list)//2:]
+conteiner = [first_part_of_big_list, second_part_of_big_list]
